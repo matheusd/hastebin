380ee14b3e63a14e3abb655fed135599ab77d83f
Matheus Degiovani <opensource@matheusd.com>
2020-08-29 16:28:40 -0300

    Godoc nits

--- doc.go
+++ doc.go
@@ -1,16 +1,12 @@
 /**
 hastebin is a CLI tool to upload text files to the hastebin.com pastebin service.
 
-## Installation
+Installation
 
-```shell
-$ go get matheusd.com/hastebin
-```
+ go get matheusd.com/hastebin
 
-## Usage
+Usage
 
-```shell
-echo "Whoozah!" | hastebin
-```
+ echo "Whoozah!" | hastebin
 */
 package main
